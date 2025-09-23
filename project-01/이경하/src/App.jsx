2cfec396dcbcde73f44311c8cd0886a5116f3df7
k0nghaa <k0nghaalee@gmail.com>
2025-09-23 14:34:11 +0900

    feat: Step3 구현 완료
    WORD 동적 입력 + 동일 로직 유지

--- project-01/이경하/src/App.jsx
+++ project-01/이경하/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 
-const WORDS = 'apple';
-
 function App() {
-  const [value, setValue] = useState('pepper');
+  const [value, setValue] = useState('');
+  const [targetValue, setTargetValue] = useState('apple');
 
   const onChangeInput = (e) => {
     setValue(e.target.value);
   };
 
-  const wordsArr = WORDS.split('');
+  const onChangeTargetWord = (e) => {
+    setTargetValue(e.target.value);
+  };
+
+  const wordsArr = targetValue.split('');
   const wordsObj = wordsArr.reduce((acc, cur) => {
     acc[cur] = (acc[cur] || 0) + 1;
     return acc;
@@ -33,21 +36,23 @@ function App() {
 
   let charCount = isObjEqual(wordsObj, inputObj);
 
-  function isEqualWords(WORDS, value) {
-    if (String(WORDS) === String(value)) {
+  function isEqualWords(targetValue, value) {
+    if (String(targetValue) === String(value)) {
       return '🎉 Collect!';
     }
     return;
   }
 
-  let equalWords = isEqualWords(WORDS, value);
+  let equalWords = isEqualWords(targetValue, value);
 
   return (
     <div className="app">
       <p>같은 글자수 : {charCount}</p>
       <p>입력값과 WORDS 가 동일한가? : {equalWords} </p>
-      <p>대상 문자 : {WORDS}</p>
-      <p>input Text : </p>
+      {/* <p>대상 문자 : {WORDS}</p> */}
+      <p>Target WORD : </p>
+      <input value={targetValue} onChange={onChangeTargetWord}></input>
+      <p>Your Input : </p>
       <input value={value} onChange={onChangeInput} />
     </div>
   );
